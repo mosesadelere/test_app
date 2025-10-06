486915bb4ada91df3eabc6e2320eb7292e3acff8
moses adelere <moses.adelere@gmail.com>
2025-10-06 22:01:45 +0300

    remove scanning

---
+++ tests/testing_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    return app.test_client()
+
+def test_hello_endpoint(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello from Simple Flask App!" in response.data
